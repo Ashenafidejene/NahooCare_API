715ea9f08c0ed145a7e423ab412b03c6c7431c5e
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-15 00:53:14 +0300

    feat: refactor health profile and rating controllers to use current user context for operations

--- controllers/healthprofile_controller.py
+++ controllers/healthprofile_controller.py
@@ -7,15 +7,15 @@ router = APIRouter()
 
 
 @router.post("/create")
-async def create(user_id:str , profile: HealthProfileCreate):
-    result = await create_health_profile(user_id,profile)
+async def create(profile: HealthProfileCreate,current_user: dict = Depends(get_current_user)):
+    result = await create_health_profile(current_user.get("user_id"),profile)
     if result:
         return "Account create success Fully"
     raise HTTPException(status_code=400, detail="Failed to create health profile")
 
 
 @router.get("/{profile_id}")
-async def get_profile(profile_id: str):
+async def get_profile(profile_id: str,current_user: dict = Depends(get_current_user)):
     profile = await get_health_profile(profile_id)
     if profile:
         return profile
@@ -23,12 +23,12 @@ async def get_profile(profile_id: str):
 
 
 @router.put("/{profile_id}")
-async def update_profile(profile_id: str, update_data: HealthProfileUpdate):
+async def update_profile(profile_id: str, update_data: HealthProfileUpdate,current_user: dict = Depends(get_current_user)):
     updated = await update_health_profile(profile_id, update_data)
     return updated
 
 
 @router.delete("/{profile_id}")
-async def delete_profile(profile_id: str):
+async def delete_profile(profile_id: str,current_user: dict = Depends(get_current_user)):
     deleted = await delete_health_profile(profile_id)
     return deleted
--- controllers/rating_controller.py
+++ controllers/rating_controller.py
@@ -2,27 +2,25 @@ from fastapi import APIRouter, HTTPException, Depends
 from schemas.rating_schema import RatingCreate,RatingUpdate
 from services.rating_service import recalculate_healthcare_rating, remove_inappropriate_review, submit_rating, get_ratings ,update_rating
 from middleware.auth import get_current_user
+from middleware.admin_auth import get_current_admin
 
 router = APIRouter()
 
 
 @router.post("/submit")
-async def rate(rating: RatingCreate):
-    result = await submit_rating(rating)
+async def rate(user_id:str,rating: RatingCreate, current_user: dict = Depends(get_current_user)):
+    result = await submit_rating(current_user["user_id"],rating=RatingCreate)
     if result : 
         return {"message": "Rating submitted successfully"}
-
-
 @router.get("/{center_id}")
 async def get_center_ratings(center_id: str):
     return await get_ratings(center_id)
-
 @router.put("/{center_id}")
 async def edit_rating(center_id: str, update_data: RatingUpdate, current_user: dict = Depends(get_current_user)):
     return await update_rating(current_user["user_id"], center_id, update_data)
 @router.delete("{rating_id}")
-async def delete_review(rating_id: str, current_user: dict = Depends(get_current_user)):
+async def delete_review(rating_id: str ,current_admin: dict = Depends(get_current_admin)):
     return await remove_inappropriate_review(rating_id)
 @router.post("/recalculate-rating/{center_id}")
-async def recalculate_rating(center_id: str, current_user: dict = Depends(get_current_user)):
+async def recalculate_rating(center_id: str, current_admin: dict = Depends(get_current_admin)):
     return await recalculate_healthcare_rating(center_id)
