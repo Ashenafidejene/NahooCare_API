f2a9184001c312bed2ae8adaceba28417a2f0079
Ashenafidejene <ashenafidejene75@gmail.com>
2025-04-15 00:54:21 +0300

    feat: update token logging and return payload in get_current_user function

--- middleware/auth.py
+++ middleware/auth.py
@@ -3,14 +3,16 @@ from fastapi.security import OAuth2PasswordBearer
 import jwt
 from core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/account/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/account/login")
+
 
 def get_current_user(token: str = Security(oauth2_scheme)):
-    print(f"Token received: {token}")
+    print(f"Token received form user : {token}")
     try:
         payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=["HS256"])
-        return payload["sub"]
+        return payload
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token expired")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
