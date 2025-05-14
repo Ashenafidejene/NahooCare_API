1e11237837a566355465947d81b050c62a87c74a
Ashenafidejene <ashenafidejene75@gmail.com>
2025-05-15 00:50:48 +0300

    fix: comment out user_id field in SearchMaterial schema

--- schemas/end_point_schemas.py
+++ schemas/end_point_schemas.py
@@ -7,7 +7,7 @@ class SearchMaterial(BaseModel):
     symptom: str = Field(..., example="Fever,Cough,Fatigue")
     latitude: float = Field(..., example=9.021)
     longitude: float = Field(..., example=38.7485)
-    user_id: str = Field(..., example="User_id_09786864r2345243")
+    #user_id: str = Field(..., example="User_id_09786864r2345243")
     max_distance_km: int = Field(default=10, example=10)
 class Respons(BaseModel):
     first_aid :Dict[str, str] = Field(...,examples=[["drink cold water" , "rub your chest"]]) 
